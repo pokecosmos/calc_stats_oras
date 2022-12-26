fb9d43b028720d12e65ef3ff0a18d64ff7ec7f70
cosmos <cosmos.8911@gmail.com>
2022-12-26 19:19:31 +0900

    個体値リセット

--- js/check_oras.js
+++ js/check_oras.js
@@ -452,4 +452,13 @@ function hiraganaToKatakana(src) {
 		return String.fromCharCode(chr);
 	});
 }
+//--個体値リセット
+function kotaiti_riset(){
+  for(i=0; i<6; i++){
+    document.nForm.elements[kn[i]].value = 31;
+  }
+  k_color();
+}
+
+
 
